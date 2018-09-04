240f4463c7a7a7051261a745a6330674aaf33868
Larry Laski <larry.laski@gmail.com>
2018-09-04 14:07:29 -0400

    Added initial preset setup w/ JS package updates and gitignore stub

---
+++ src/PresetServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BuiltByLarry\LaravelPreset;
+
+use Illuminate\Foundation\Console\PresetCommand;
+use Illuminate\Support\ServiceProvider;
+
+class PresetServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        PresetCommand::macro('BuiltByLarry', function ($command) {
+            Preset::install();
+
+            $command->info('BuiltByLarry scaffolding installed successfully.');
+            $command->info('Please run "npm install && npm run dev" to compile your fresh scaffolding.');
+        });
+    }
+}
